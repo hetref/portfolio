4beefb44860decce4bb81bbdbd5ee21e81540e0c
Aryan shinde <shindearyan179@gmail.com>
2024-03-25 12:30:20 +0530

    Updated SEO and Added Favicon

--- app/layout.js
+++ app/layout.js
@@ -7,23 +7,127 @@ const inter = Inter({ subsets: ["latin"] });
 
 // Add SEO with keywords to this page
 
-export const metadata = {
-  title: "Aryan Shinde | Portfolio",
-  description:
-    "Welcome to my portfolio, here you'll get all my creatives which I've made till date!",
-  keywords:
-    "Aryan Shinde, Portfolio, Aryan Shinde Portfolio, Aryan Shinde Projects, Aryan Shinde Skills, Aryan Shinde Experience, Aryan Shinde Contact, Aryan Shinde Resume",
-};
-
 // export const metadata = {
 //   title: "Aryan Shinde | Portfolio",
-//   description: "Here's the list of the things I've done to showcase myself!",
+//   description:
+//     "Welcome to my portfolio, here you'll get all my creatives which I've made till date!",
+//   keywords:
+//     "Aryan Shinde, Portfolio, Aryan Shinde Portfolio, Aryan Shinde Projects, Aryan Shinde Skills, Aryan Shinde Experience, Aryan Shinde Contact, Aryan Shinde Resume",
 // };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
+        <title>Aryan Shinde | Portfolio</title>
+        {/* Meta Data */}
+        <meta name="title" content="Aryan Shinde | Portfolio" />
+        <meta
+          name="description"
+          content="Welcome to my portfolio, here you'll get all the creatives I've made to date!"
+        />
+        <meta
+          name="keywords"
+          content="Aryan Shinde, Portfolio, Aryan Shinde Portfolio, Aryan Shinde Projects, Aryan Shinde Skills, Aryan Shinde Experience, Aryan Shinde Contact, Aryan Shinde Resume"
+        />
+
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://www.aryanshinde.in/" />
+        <meta property="og:title" content="Aryan Shinde | Portfolio" />
+        <meta
+          property="og:description"
+          content="Welcome to my portfolio, here you'll get all the creatives I've made to date!"
+        />
+        <meta property="og:image" content="/logo.png" />
+
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:url" content="https://www.aryanshinde.in/" />
+        <meta property="twitter:title" content="Aryan Shinde | Portfolio" />
+        <meta
+          property="twitter:description"
+          content="Welcome to my portfolio, here you'll get all the creatives I've made to date!"
+        />
+        <meta property="twitter:image" content="/logo.png" />
+
+        {/* Favicon */}
+
+        <link
+          rel="apple-touch-icon"
+          sizes="57x57"
+          href="/favicon/apple-icon-57x57.png"
+        ></link>
+        <link
+          rel="apple-touch-icon"
+          sizes="60x60"
+          href="/favicon/apple-icon-60x60.png"
+        ></link>
+        <link
+          rel="apple-touch-icon"
+          sizes="72x72"
+          href="/favicon/apple-icon-72x72.png"
+        ></link>
+        <link
+          rel="apple-touch-icon"
+          sizes="76x76"
+          href="/favicon/apple-icon-76x76.png"
+        ></link>
+        <link
+          rel="apple-touch-icon"
+          sizes="114x114"
+          href="/favicon/apple-icon-114x114.png"
+        ></link>
+        <link
+          rel="apple-touch-icon"
+          sizes="120x120"
+          href="/favicon/apple-icon-120x120.png"
+        ></link>
+        <link
+          rel="apple-touch-icon"
+          sizes="144x144"
+          href="/favicon/apple-icon-144x144.png"
+        ></link>
+        <link
+          rel="apple-touch-icon"
+          sizes="152x152"
+          href="/favicon/apple-icon-152x152.png"
+        ></link>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/favicon/apple-icon-180x180.png"
+        ></link>
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="192x192"
+          href="/favicon/android-icon-192x192.png"
+        ></link>
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon/favicon-32x32.png"
+        ></link>
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="96x96"
+          href="/favicon/favicon-96x96.png"
+        ></link>
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon/favicon-16x16.png"
+        ></link>
+        <link rel="manifest" href="/favicon/manifest.json"></link>
+        <meta name="msapplication-TileColor" content="#ffffff" />
+        <meta
+          name="msapplication-TileImage"
+          content="/favicon/ms-icon-144x144.png"
+        />
+        <meta name="theme-color" content="#ffffff" />
+
         <script
           defer
           data-domain="portfolio-hetref.vercel.app"
