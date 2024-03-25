7f626354fdb2734a7352577fad4befc043a6fdde
Aryan shinde <shindearyan179@gmail.com>
2024-03-25 12:04:23 +0530

    Added SEO

--- app/layout.js
+++ app/layout.js
@@ -5,10 +5,20 @@ import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const inter = Inter({ subsets: ["latin"] });
 
+// Add SEO with keywords to this page
+
 export const metadata = {
   title: "Aryan Shinde | Portfolio",
-  description: "Here's the list of the things I've done to showcase myself!",
+  description:
+    "Welcome to my portfolio, here you'll get all my creatives which I've made till date!",
+  keywords:
+    "Aryan Shinde, Portfolio, Aryan Shinde Portfolio, Aryan Shinde Projects, Aryan Shinde Skills, Aryan Shinde Experience, Aryan Shinde Contact, Aryan Shinde Resume",
 };
+
+// export const metadata = {
+//   title: "Aryan Shinde | Portfolio",
+//   description: "Here's the list of the things I've done to showcase myself!",
+// };
 
 export default function RootLayout({ children }) {
   return (
